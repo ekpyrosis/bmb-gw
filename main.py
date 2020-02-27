85ab0c3043b2c9fbae08da4798fc6924208b7e8f
ekpyrosis <ekpyrosis@gmail.com>
2020-02-27 11:31:28 +0300

    ready2prod

--- main.py
+++ main.py
@@ -2,18 +2,15 @@ from japronto import Application
 from rethinkdb import RethinkDB
 
 r = RethinkDB()
-
 r.connect("localhost", 28015).repl()
 
 
 def generic(request):
-    print(request.json)
+    # print(request.json)
     r.db("geo").table('location').insert(request.json).run()
     return request.Response(json={"result": "ok"})
 
 
 app = Application()
-
 app.router.add_route('/', generic)
-
-app.run(port=262, debug=True)
+app.run(port=262, debug=False)
